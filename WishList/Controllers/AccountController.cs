eed1c8612cfbde6e94b0d73c7bdf1166bec6ebad
Kwarteng Isaac Divinus <kwarteng.divinus@gmail.com>
2021-12-27 10:19:46 +0000

    Completed module

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -37,22 +37,54 @@ namespace WishList.Controllers
             //var userName = await _userManager.FindByNameAsync(model.Email);
             //userName = await _userManager.FindByEmailAsync(model.Email);
             var result = _userManager.CreateAsync(
-                new ApplicationUser() { Email = model.Email, UserName = model.Email },model.Password).Result;
+                new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
             //if (userName == null)
             //{
             //    await _userManager.SetUserNameAsync(userName, model.Email);
             //    await _userManager.SetEmailAsync(userName, model.Email);
             //    var result = await _userManager.CreateAsync(userName, model.Password);
-                if (!result.Succeeded)
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
                 {
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError("Password", error.Description);
-                    }
-                        return View(model);
+                    ModelState.AddModelError("Password", error.Description);
                 }
-               // return View();
-           // }
+                return View(model);
+            }
+            // return View();
+            // }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var result = _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false).Result;
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login credentials");
+
+                return View(model);
+            }
+            return RedirectToAction("Index", "Item");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
     }
