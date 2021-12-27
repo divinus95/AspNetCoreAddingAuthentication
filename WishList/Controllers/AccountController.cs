a493deaaaf13f47bd9b86bd21ab5f889ffecd4bd
Kwarteng Isaac Divinus <kwarteng.divinus@gmail.com>
2021-12-27 09:29:17 +0000

    Completed module

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
@@ -19,6 +21,39 @@ namespace WishList.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var userName = await _userManager.FindByNameAsync(model.Email);
+            userName = await _userManager.FindByEmailAsync(model.Email);
+
+            if (userName == null)
+            {
+                await _userManager.SetUserNameAsync(userName, model.Email);
+                await _userManager.SetEmailAsync(userName, model.Email);
+                var result = await _userManager.CreateAsync(userName, model.Password);
+                if (!result.Succeeded)
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("Password", "Password Error");
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+               // return View();
+            }
+            return RedirectToAction(nameof(Index));
+
+
         }
     }
 }
